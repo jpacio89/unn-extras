a2ba20c8d79592a92220ee59aca596f5b1eb05dd
Joao Coelho <joao.coelho@bontouch.com>
2020-02-12 20:11:26 +0100

    no message

--- stock-influencers/get-trades.php
+++ stock-influencers/get-trades.php
@@ -88,49 +88,4 @@
         return $investors;
     }
 
-    function extractCookies($string) {
-        $lines = explode(PHP_EOL, $string);
-
-        foreach ($lines as $line) {
-
-            $cookie = array();
-
-            // detect httponly cookies and remove #HttpOnly prefix
-            if (substr($line, 0, 10) == '#HttpOnly_') {
-                $line = substr($line, 10);
-                $cookie['httponly'] = true;
-            } else {
-                $cookie['httponly'] = false;
-            }
-
-            // we only care for valid cookie def lines
-            if( strlen( $line ) > 0 && $line[0] != '#' && substr_count($line, "\t") == 6) {
-
-                // get tokens in an array
-                $tokens = explode("\t", $line);
-
-                // trim the tokens
-                $tokens = array_map('trim', $tokens);
-
-                // Extract the data
-                $cookie['domain'] = $tokens[0]; // The domain that created AND can read the variable.
-                $cookie['flag'] = $tokens[1];   // A TRUE/FALSE value indicating if all machines within a given domain can access the variable.
-                $cookie['path'] = $tokens[2];   // The path within the domain that the variable is valid for.
-                $cookie['secure'] = $tokens[3]; // A TRUE/FALSE value indicating if a secure connection with the domain is needed to access the variable.
-
-                $cookie['expiration-epoch'] = $tokens[4];  // The UNIX time that the variable will expire on.
-                $cookie['name'] = urldecode($tokens[5]);   // The name of the variable.
-                $cookie['value'] = urldecode($tokens[6]);  // The value of the variable.
-
-                // Convert date to a readable format
-                $cookie['expiration'] = date('Y-m-d h:i:s', $tokens[4]);
-
-                // Record the cookie.
-                $cookies[] = $cookie;
-            }
-        }
-
-        return $cookies;
-    }
-
 ?>
