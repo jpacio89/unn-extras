7c6c96b690e68dfc28457abe734f27e8b5d21eb2
Joao Coelho <joao.coelho@bontouch.com>
2020-02-12 23:11:27 +0100

    no message

--- stock-influencers/get-trades.php
+++ stock-influencers/get-trades.php
@@ -1,19 +1,29 @@
 <?php
 
     $investors = getInvestors();
-    $trades = [];
     $investorCount = count($investors);
+    $summary = array();
 
     for ($i = 0; $i < $investorCount; ++$i) {
         echo "$i / $investorCount\n";
-        $localTrades = getTrades($investors[$i], $i);
-        $trades = array_merge($trades, $localTrades);
+        $investor = $investors[$i];
+        $investorId = $investor['CustomerId'];
+        $trades = getTrades($investor, $i);
+        $summary[$investorId] = getSummary($trades);
+        print_r($trades);
+        file_put_contents('data/trades-'.$investorId.'.investor.json', json_encode($trades));
+        file_put_contents('data/trades.summary.json', json_encode($summary));
         sleep(3);
     }
 
-    print_r($trades);
-
-    file_put_contents('data/trades.json', json_encode($trades));
+    function getSummary($trades) {
+        $vals = array();
+        for ($i = 0; $i < count($trades); ++$i) {
+            $trade = $trades[$i];
+            $vals[$trade['InstrumentID']]++;
+        }
+        return $vals;
+    }
 
     function getTrades($user, $curlIndex = 0) {
         global $cookie;
@@ -67,7 +77,7 @@
         $investors = [];
 
         //$json = file_get_contents('https://www.etoro.com/sapi/rankings/rankings/?activeweeksmin=40&blocked=false&bonusonly=false&client_request_id=cd8928c2-7fc8-4291-913f-0633e12c32bf&copyblock=false&copyinvestmentpctmax=5&copytradespctmax=5&dailyddmin=-7&gainmax=80&gainmin=5&hasavatar=true&istestaccount=false&lastactivitymax=31&maxmonthlyriskscoremax=4&maxmonthlyriskscoremin=1&optin=true&page=1&pagesize=200&period=OneYearAgo&profitablemonthspctmin=55&profitableweekspctmin=55&sort=-weeklydd&tradesmin=25&verified=true&weeklyddmin=-14');
-        $json = file_get_contents('https://www.etoro.com/sapi/rankings/rankings/?blocked=false&bonusonly=false&client_request_id=35bd969d-650e-40a1-b082-9970109dc318&copyblock=false&istestaccount=false&lastactivitymax=30&optin=true&page=1&pagesize=100&period=OneYearAgo&sort=-gain&tradesmin=500&verified=true');
+        $json = file_get_contents('https://www.etoro.com/sapi/rankings/rankings/?blocked=false&bonusonly=false&client_request_id=35bd969d-650e-40a1-b082-9970109dc318&copyblock=false&istestaccount=false&lastactivitymax=30&optin=true&page=1&pagesize=300&period=OneYearAgo&sort=-gain&tradesmin=500&verified=true');
 
         $json = json_decode($json, true);
 
