bd81a7616d97f353b972f2ec957a12b4e70389ab
Joao Coelho <joao.coelho@bontouch.com>
2020-02-13 21:10:50 +0100

    no message

--- stock-influencers/get-trades.php
+++ stock-influencers/get-trades.php
@@ -1,23 +1,27 @@
 <?php
-
-    $investors = getInvestors();
-    $investorCount = count($investors);
     $summary = array();
 
-    for ($i = 0; $i < $investorCount; ++$i) {
-        echo "$i / $investorCount\n";
-        $investor = $investors[$i];
-        $investorId = $investor['CustomerId'];
-        $trades = getTrades($investor, $i);
-        $summary[] = array(
-            "investor" => $investorId,
-            "counts"   => getSummary($trades)
-        );
-        print_r($trades);
-        file_put_contents('data/trades-'.$investorId.'.investor.json', json_encode($trades));
-        file_put_contents('data/trades.summary.json', json_encode($summary));
-        sleep(3);
+    for ($page = 1; $page < 5; ++$page) {
+        $investors = getInvestors($page);
+        $investorCount = count($investors);
+        // $summary = array();
+
+        for ($i = 0; $i < $investorCount; ++$i) {
+            echo "$page -> $i / $investorCount\n";
+            $investor = $investors[$i];
+            $investorId = $investor['CustomerId'];
+            $trades = getTrades($investor, $i);
+            $summary[] = array(
+                "investor" => $investorId,
+                "counts"   => getSummary($trades)
+            );
+            // print_r($trades);
+            file_put_contents('data/trades-'.$investorId.'.investor.json', json_encode($trades));
+            file_put_contents('data/trades.summary.json', json_encode($summary));
+            sleep(3);
+        }
     }
+
 
     function getSummary($trades) {
         $vals = array();
@@ -45,7 +49,7 @@
         $json = @json_decode($output, true);
 
         if (!isset($json['PublicHistoryPositions'])) {
-            echo "[WARNING] Empty trades: $output\n";
+            echo "[WARNING] Empty trades @User = $user: $output\n";
             return $trades;
         }
 
@@ -76,11 +80,11 @@
         return $time;
     }
 
-    function getInvestors() {
+    function getInvestors($page = 1) {
         $investors = [];
 
         //$json = file_get_contents('https://www.etoro.com/sapi/rankings/rankings/?activeweeksmin=40&blocked=false&bonusonly=false&client_request_id=cd8928c2-7fc8-4291-913f-0633e12c32bf&copyblock=false&copyinvestmentpctmax=5&copytradespctmax=5&dailyddmin=-7&gainmax=80&gainmin=5&hasavatar=true&istestaccount=false&lastactivitymax=31&maxmonthlyriskscoremax=4&maxmonthlyriskscoremin=1&optin=true&page=1&pagesize=200&period=OneYearAgo&profitablemonthspctmin=55&profitableweekspctmin=55&sort=-weeklydd&tradesmin=25&verified=true&weeklyddmin=-14');
-        $json = file_get_contents('https://www.etoro.com/sapi/rankings/rankings/?blocked=false&bonusonly=false&client_request_id=35bd969d-650e-40a1-b082-9970109dc318&copyblock=false&istestaccount=false&lastactivitymax=30&optin=true&page=1&pagesize=300&period=OneYearAgo&sort=-gain&tradesmin=500&verified=true');
+        $json = file_get_contents('https://www.etoro.com/sapi/rankings/rankings/?blocked=false&bonusonly=false&client_request_id=35bd969d-650e-40a1-b082-9970109dc318&copyblock=false&istestaccount=false&lastactivitymax=30&optin=true&page='.$page.'&pagesize=200&period=OneYearAgo&sort=-gain&tradesmin=500&verified=true');
 
         $json = json_decode($json, true);
 
